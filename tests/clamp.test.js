f610a721cd54d6da777a9e8f727d568311dbcc64
John <johnmoreau19@gmail.com>
2024-02-11 17:15:25 -0800

    Added first test for clamp

--- tests/clamp.test.js
+++ tests/clamp.test.js
@@ -1,8 +1,26 @@
-const clamp = require('../src/clamp');
+const clamp = require("../src/clamp");
 
-describe('clamp', () => {
-  // TODO: Put your tests here
-  // Test that defaults are applied
-  // Test that lower clamps are applied
-  // Test that upper clamps are applied
+describe("clamp", () => {
+    // TODO: Put your tests here
+
+    // Test that lower clamps are applied
+    // Test that upper clamps are applied
+
+    // Test that defaults are applied
+    test("should apply defaults", () => {
+        let result = clamp(10);
+        expect(result).toBe(1);
+        result = clamp(-10);
+        expect(result).toBe(0);
+    });
+
+    test("should apply lower clamps", () => {
+        let result = clamp(-10, -5);
+        expect(result).toBe(-5);
+    });
+
+    test("should apply upper clamps", () => {
+        const result = clamp(10, undefined, 5);
+        expect(result).toBe(5);
+    });
 });
