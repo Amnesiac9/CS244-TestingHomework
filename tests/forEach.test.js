0d202a6bed012b4c45d9ef748ef0fe77dce58e9d
John <johnmoreau19@gmail.com>
2024-02-11 18:01:16 -0800

    Added forEach tests and query network tests

--- tests/forEach.test.js
+++ tests/forEach.test.js
@@ -1,7 +1,20 @@
-const forEach = require('../src/forEach');
+const forEach = require("../src/forEach");
 
-describe('forEach', () => {
-  // TODO: Put your tests here
-  // Test that your callback is invoked for each array entry
-  // Test that your callback arguments are provided accurately
+describe("forEach", () => {
+    // Test that your callback is invoked for each array entry
+    test("should callback for each array entry", () => {
+        const mockCallback = jest.fn();
+        forEach([1, 2, 3], mockCallback);
+        expect(mockCallback.mock.calls.length).toBe(3);
+    });
+    // Test that your callback arguments are provided accurately
+    test("should callback with arguments", () => {
+        const mockCallback = jest.fn();
+        forEach([1, 2, 3], mockCallback);
+
+        // Check the args passed to each call of the callback
+        expect(mockCallback.mock.calls[0][0]).toBe(1);
+        expect(mockCallback.mock.calls[1][0]).toBe(2);
+        expect(mockCallback.mock.calls[2][0]).toBe(3);
+    });
 });
--- tests/queryNetwork.test.js
+++ tests/queryNetwork.test.js
@@ -1,9 +1,25 @@
-const queryNetwork = require('../src/queryNetwork');
+const fetch = require("../dependencies/fetch");
+const queryNetwork = require("../src/queryNetwork");
 
-describe('queryNetwork', () => {
-  // TODO: Put your tests here
-  // Mock fetch.js
-  // Test that fetch is invoked
-  // Test that the returned promise resolves into the object
-  // your mock is using for the json() method
+// Not sure if I did this right.
+jest.mock("../dependencies/fetch");
+
+describe("queryNetwork", () => {
+    // Mock fetch.js
+    // const mockJson = jest.fn(() => ({}));
+    const mockFetch = jest.fn(() => Promise.resolve({ json: () => ({ json: { test: "" } }) }));
+    // Test that fetch is invoked
+    test("should call fetch", async () => {
+        fetch.mockImplementation(mockFetch);
+        await queryNetwork("https://test.com");
+        expect(fetch).toBeCalled();
+    });
+
+    // Test that the returned promise resolves into the object
+    // your mock is using for the json() method
+    test("should return the object from json", async () => {
+        fetch.mockImplementation(mockFetch);
+        const result = await queryNetwork("https://test.com");
+        expect(result).toEqual({ json: { test: "" } });
+    });
 });
